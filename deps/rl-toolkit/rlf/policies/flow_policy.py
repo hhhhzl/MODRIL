271039306386fa2a67b0a2fc75763d502f558d28
root <hectorh@andrew.cmu.edu>
2025-06-15 01:44:01 +0000

    fix bcf

--- deps/rl-toolkit/rlf/policies/flow_policy.py
+++ deps/rl-toolkit/rlf/policies/flow_policy.py
@@ -1,6 +1,22 @@
 import torch
 from rlf.policies.base_net_policy import BaseNetPolicy
+import rlf.rl.utils as rutils
 
+class CreateAction():
+    def __init__(self, action):
+        self.action = action
+        self.hxs = {}
+        self.extra = {}
+        self.take_action = action
+        
+    def clip_action(self, low_bound, upp_bound):
+        # When CUDA is enabled the action will be on the GPU.
+        self.action = rutils.multi_dim_clip(
+            self.action,
+            low_bound.to(self.action.device),
+            upp_bound.to(self.action.device),
+        )
+        self.take_action = rutils.multi_dim_clip(self.take_action, low_bound, upp_bound)
 
 class FlowPolicy(BaseNetPolicy):
     """
@@ -9,20 +25,51 @@ class FlowPolicy(BaseNetPolicy):
 
     def __init__(self, fuse_states=[], use_goal=False, get_base_net_fn=None):
         super().__init__(use_goal, fuse_states, get_base_net_fn)
+        self.state_norm_fn = lambda x: x
+        self.action_denorm_fn = lambda x: x
+
+    def set_state_norm_fn(self, state_norm_fn):
+        self.state_norm_fn = state_norm_fn
+
+    def set_action_denorm_fn(self, action_denorm_fn):
+        self.action_denorm_fn = action_denorm_fn
 
     def init(self, obs_shape, action_space, args):
-        super().init(obs_shape, action_space, args)
-        state_feat_dim = self.base_net.output_shape[0]
-        action_dim = action_space.shape[0]
+        # super().init(obs_shape, action_space, args)
+        self.args = args
+        self.obs_space, self.action_space = obs_shape, action_space
         self.velocity_net = self.get_base_net_fn
-        self.velocity_net.state_dim = state_feat_dim
-        self.velocity_net.action_dim = action_dim
-        self.velocity_net.to(self.device)
         self.optimizer = torch.optim.Adam(self.velocity_net.parameters(), lr=1e-4)
 
-    def get_actions(self, obs, rnn_hxs, masks, **kwargs):
-        x_t = kwargs["x_t"]
-        t = kwargs["t"]
-        state_feat, hxs = self.base_net(obs, rnn_hxs, masks)
-        v_pred = self.velocity_net(x_t, state_feat, t)
-        return v_pred, hxs, {}
+    def get_add_args(self, parser):
+        parser.add_argument(
+            "--flow_integration_steps", action="store_true", default=32
+        )
+
+    def get_action(self, state, add_state, rnn_hxs, mask, step_info):
+        device = self.args.device
+        batch_size = state.shape[0]
+        action_dim = self.velocity_net.action_dim
+
+        x = torch.randn(batch_size, action_dim, device=device)
+        n_steps = getattr(self.args, "flow_integration_steps", 32)
+        dt = 1.0 / n_steps
+
+        for i in range(n_steps):
+            t = torch.full((batch_size, 1), fill_value=i * dt, device=device)
+            v = self.forward(x, state, t)  
+            x = x + v * dt
+
+        return CreateAction(x)
+
+
+    def get_storage_hidden_states(self):
+        hxs = {}
+        hxs['hxs'] = self.velocity_net.hidden_dim
+        hxs['txs'] = self.velocity_net.time_dim
+        return hxs
+
+
+    def forward(self, x_t: torch.Tensor, s: torch.Tensor, t: torch.Tensor):
+        return self.velocity_net(x_t, s, t)
+        
